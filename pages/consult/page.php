728d98ea3e4ba1f880d460e4a4b42b3858b3d082
airman5573 <rpf5573@kakao.com>
2021-11-23 22:07:33 +0900

    문의 페이지까지 완료

--- pages/consult/page.php
+++ pages/consult/page.php
@@ -6,16 +6,18 @@
       환영합니다.
     </p>
   </div>
-  <form action="">
+  <form name="apply_form" method="post" enctype="multipart/form-data" action="apply_process.php?">
     <div class="form-row">
-      <label class="required mb-15">구분</label>
+      <label class="required mb-20">구분</label>
       <div class="form-field">
-        <div class="field-container mr-40">
-          <input id="customer-type--new" name="type" type="radio" required>
+        <div class="field-container mr-30">
+          <input id="customer-type--new" name="type" type="radio">
+          <div class="checkbox"></div>
           <label for="customer-type--new" class="fw-400">신규 고객</label>
         </div>
         <div class="field-container">
-          <input id="customer-type--prolong" name="type" type="radio" required>
+          <input id="customer-type--prolong" name="type" type="radio">
+          <div class="checkbox"></div>
           <label for="customer-type--prolong" class="fw-400">연장 고객</label>
         </div>
       </div>
@@ -23,7 +25,7 @@
     <div class="form-row">
       <label for="company" class="required">회사명</label>
       <div class="form-field">
-        <input id="company" type="text" name="company" placeholder="회사명을 입력해 주세요." required>
+        <input id="company" type="text" name="company" placeholder="회사명을 입력해 주세요.">
       </div>
     </div>
     <div class="form-row">
@@ -47,59 +49,66 @@
     <div class="form-row">
       <label for="tel" class="required">연락처</label>
       <div class="form-field">
-        <input id="tel" name="tel" type="text" placeholder="연락처를 입력해 주세요.(-없이 숫자만 입력)">
+        <input id="tel" name="tel" type="text" placeholder="연락처를 입력해 주세요.(-없이 숫자만 입력)" oninput="this.value = this.value.replace(/[^0-9]/g, '');">
       </div>
     </div>
     <div class="form-row">
       <label for="email1" class="required">이메일</label>
       <div class="form-field form-field--email input-highlight" tabindex="1">
-        <input id="email1" name="email1" type="text" required/><span>@</span><input id="email2" name="email2" type="text" disabled value="naver.com">
-        <select name="select-email" id="select-email">
-          <option value="1">직접입력</option>
-          <option value="naver.com" selected>naver.com</option>
-          <option value="hanmail.net">hanmail.net</option>
-          <option value="hotmail.com">hotmail.com</option>
-          <option value="nate.com">nate.com</option>
-          <option value="yahoo.co.kr">yahoo.co.kr</option>
-          <option value="empas.com">empas.com</option>
-          <option value="dreamwiz.com">dreamwiz.com</option>
-          <option value="freechal.com">freechal.com</option>
-          <option value="lycos.co.kr">lycos.co.kr</option>
-          <option value="korea.com">korea.com</option>
-          <option value="gmail.com">gmail.com</option>
-          <option value="hanmir.com">hanmir.com</option>
-          <option value="paran.com">paran.com</option>
-        </select>
+        <input id="email1" name="email1" type="text"/><span>@</span><input id="email2" name="email2" type="text" disabled value="naver.com">
+        <ul class="emails box-shadow-1">
+          <li rel="self">직접입력</li>
+          <li rel="naver.com">naver.com</li>
+          <li rel="kakao.com">kakao.com</li>
+          <li rel="gmail.com">gmail.com</li>
+          <li rel="hanmail.net">hanmail.net</li>
+          <li rel="nate.com">nate.com</li>
+        </ul>
+        <div class="arrow-down" tabindex="2">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="12" viewBox="0 0 12 8" fill="none">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M11.3778 0.774584C11.7073 1.1407 11.7073 1.7343 11.3778 2.10041L6.59662 7.41287C6.26712 7.779 5.73288 7.779 5.40338 7.41287L0.622126 2.10041C0.292625 1.7343 0.292625 1.1407 0.622126 0.774584C0.951627 0.408472 1.48587 0.408472 1.81537 0.774584L6 5.42417L10.1847 0.774584C10.5142 0.408472 11.0483 0.408472 11.3778 0.774584Z" fill="#24292E"/>
+          </svg>
+        </div>
       </div>
     </div>
     <div class="form-row">
-      <label for="position">상담구분</label>
+      <label for="position" class="d-block mb-15">상담구분</label>
       <div class="form-field">
-        <div class="field-container">
-          <input id="consult-type-1" name="sec1[]" type="checkbox">
-          <label for="consult-type-1">국내 마케팅</label>
+        <div class="row d-flex">
+          <div class="field-container mr-20">
+            <input id="consult-type-1" class="d-none" name="sec1[]" type="checkbox">
+            <div class="checkbox"></div>
+            <label for="consult-type-1">국내 마케팅</label>
+          </div>
+          <div class="field-container">
+            <input id="consult-type-2" class="d-none" name="sec1[]" type="checkbox">
+            <div class="checkbox"></div>
+            <label for="consult-type-2">해외 마케팅</label>
+          </div>
         </div>
-        <div class="field-container">
-          <input id="consult-type-2" name="sec1[]" type="checkbox">
-          <label for="consult-type-2">해외 마케팅</label>
-        </div>
-        <div class="field-container">
-          <input id="consult-type-3" name="sec1[]" type="checkbox">
-          <label for="consult-type-3">국내 언론홍보</label>
-        </div>
-        <div class="field-container">
-          <input id="consult-type-4" name="sec1[]" type="checkbox">
-          <label for="consult-type-4">해외 언론홍보</label>
-        </div>
-        <div class="field-container">
-          <input id="consult-type-5" name="sec1[]" type="checkbox">
-          <label for="consult-type-5">기타</label>
+        <div class="row d-flex">
+          <div class="field-container mr-20">
+            <input id="consult-type-3" class="d-none" name="sec1[]" type="checkbox">
+            <div class="checkbox"></div>
+            <label for="consult-type-3">국내 언론홍보</label>
+          </div>
+          <div class="field-container mr-20">
+            <input id="consult-type-4" class="d-none" name="sec1[]" type="checkbox">
+            <div class="checkbox"></div>
+            <label for="consult-type-4">해외 언론홍보</label>
+          </div>
+          <div class="field-container">
+            <input id="consult-type-5" class="d-none" name="sec1[]" type="checkbox">
+            <div class="checkbox"></div>
+            <label for="consult-type-5">기타</label>
+          </div>
         </div>
       </div>
     </div>
-    <div class="form-row">
-      <label for="content">문의내용</label>
-      <textarea name="content" id="content" cols="30" rows="10" placeholder="문의하실 내용을 입력해주세요."></textarea>
+    <div class="form-row mb-45">
+      <label for="content" class="mb-20">문의내용</label>
+      <textarea name="content" id="content" cols="30" rows="8" placeholder="문의하실 내용을 입력해주세요."></textarea>
     </div>
+    <button type="submit">문의하기</button>
   </form>
 </div>
--- pages/consult/script.js
+++ pages/consult/script.js
@@ -5,3 +5,59 @@ emailField.focusin(() => {
 emailField.focusout(() => {
   emailField.toggleClass('active');
 });
+
+let emails = $('ul.emails');
+let arrowDown = $('.arrow-down');
+arrowDown.focusin(() => {
+  emails.toggleClass('show');
+});
+arrowDown.focusout(() => {
+  emails.toggleClass('show');
+});
+let email2 = $('#email2');
+$('ul.emails li').on('mousedown', function() {
+  const self = $(this);
+  const email = self.attr('rel');
+  if (email == 'self') {
+    email2.val('');
+    email2.attr('disabled', false);
+  } else {
+    email2.val(email);
+    email2.attr('disabled', true);
+  }
+});
+
+$('form').on('submit', function() {
+  if (!($('input#customer-type--new').prop('checked') || $('input#customer-type--prolong').prop('checked'))) {
+    alert("구분을 선택해주세요");
+    return false;
+  }
+
+  if ($('input[name=company]').val().length < 1) {
+    alert("회사명을 입력해주세요");
+    return false;
+  }
+
+  if ($('input[name=name]').val().length < 1) {
+    alert("고객명을 입력해주세요");
+    return false;
+  }
+
+  if ($('input[name=tel]').val().length < 1) {
+    alert("전화번호를 입력해주세요");
+    return false;
+  }
+
+  if ($('input[name=email1]').val().length < 1 || $('input[name=email2]').val().length < 1) {
+    alert("이메일을 입력해주세요");
+    return false;
+  }
+
+  return true;
+});
+
+function validatePhoneNumber(phoneNumber) {
+  var patternPhone = /01[016789][0-9]{4}[0-9]{4}/;
+  if (!patternPhone.test(phoneNumber)) return false;
+  return true;
+}

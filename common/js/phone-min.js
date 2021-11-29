e7df31ad0546e9c50bb105a3d5691e38d8a48c67
airman5573 <rpf5573@kakao.com>
2021-11-29 16:13:48 +0900

    fix: phone button hides share button

--- common/js/phone-min.js
+++ common/js/phone-min.js
@@ -1,1 +1,24 @@
-jQuery(document).ready((function(){let o=$(".phone");const t=$(window);function n(){t.scrollTop()+window.innerHeight+120>=document.body.scrollHeight?o.hasClass("at-bottom")||o.addClass("at-bottom"):o.hasClass("at-bottom")&&o.removeClass("at-bottom")}$(document.body).on("touchmove",n),t.on("scroll",n)}));
+jQuery(document).ready(function(){
+  let $phone = $('.phone');
+  const $window = $(window);
+  $(document.body).on('touchmove', onScroll); // for mobile
+  $window.on('scroll', onScroll); 
+  // callback
+  function onScroll(){
+    if( $window.scrollTop() + window.innerHeight + 120 >= document.body.scrollHeight ) { 
+      if (!$phone.hasClass('at-bottom')) {
+        $phone.addClass('at-bottom');
+        setTimeout(() => {
+          $phone.css('display', 'none');
+        }, 320);
+      }
+    } else {
+      if ($phone.hasClass('at-bottom')) {
+        $phone.css('display', 'block');
+        setTimeout(() => {
+          $phone.removeClass('at-bottom');
+        }, 10);
+      }
+    }
+  }
+});
--- common/js/phone.js
+++ common/js/phone.js
@@ -8,10 +8,16 @@ jQuery(document).ready(function(){
     if( $window.scrollTop() + window.innerHeight + 120 >= document.body.scrollHeight ) { 
       if (!$phone.hasClass('at-bottom')) {
         $phone.addClass('at-bottom');
+        setTimeout(() => {
+          $phone.css('display', 'none');
+        }, 320);
       }
     } else {
       if ($phone.hasClass('at-bottom')) {
-        $phone.removeClass('at-bottom');
+        $phone.css('display', 'block');
+        setTimeout(() => {
+          $phone.removeClass('at-bottom');
+        }, 10);
       }
     }
   }
